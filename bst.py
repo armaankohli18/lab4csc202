13a9453ff368c9007e191b04a0a264fb96a946c9
Ashlyn Wilson <ashlynswilson07@gmail.com>
2026-02-18 16:44:08 -0800

    lookup function & helper

--- bst.py
+++ bst.py
@@ -15,3 +15,16 @@ class Node:
 class BinarySearchTree:
     comes_before: Callable[[Any, Any], bool]
     BTree : BinTree
+
+def lookup_helper(x: Any, bst: BinTree, comes_before: Callable[[Any, Any], bool]) -> bool:
+    if bst is None:
+        return False
+    elif bst.value == x:
+        return True
+    elif comes_before(x, bst.value):
+        return lookup_helper(x, bst.left, comes_before)
+    elif comes_before(bst.value, x):
+        return lookup_helper(x, bst.right, comes_before)
+    
+def lookup(x: Any, bst: BinarySearchTree) -> bool:
+    return lookup_helper(x, bst.BTree, bst.comes_before) 
