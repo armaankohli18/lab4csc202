41b5083925651ffce5ef800bb6e702ad432f645c
armaankohli18 <armaankcollege@gmail.com>
2026-02-18 16:32:23 -0800

    did the data defs

--- bst.py
+++ bst.py
@@ -3,3 +3,15 @@ import unittest
 from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**9)
+
+BinTree = Union["Node", None]
+
+class Node:
+    value: Any
+    left: BinTree
+    right: BinTree
+
+@dataclass(frozen=True)
+class BinarySearchTree:
+    comes_before: Callable[[Any, Any], bool]
+    BTree : BinTree
